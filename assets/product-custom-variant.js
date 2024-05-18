624d8649b6324464dc1bb98432f4b6f352eed6bb
Matheus Aguiar Amaral <matheusaguiardev@gmail.com>
2024-05-17 21:32:59 -0300

    url change

--- assets/product-custom-variant.js
+++ assets/product-custom-variant.js
@@ -1,1 +1,18 @@
 console.log('hello from custom variant!');
+
+document.querySelectorAll('.product-custom-selection a').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('clicked', el.dataset.option);
+    switch (el.dataset.option) {
+      case 'white':
+        window.history.pushState('object or string', 'Title', '/products/white-t-shirt');
+        break;
+      case 'red':
+        window.history.pushState('object or string', 'Title', '/products/red-t-shirt');
+        break;
+      default:
+        window.history.pushState('object or string', 'Title', '/products/black-t-shirt');
+    }
+  });
+});
